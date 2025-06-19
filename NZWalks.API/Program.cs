182baaf93b5327b68d93a53878638ca9e0619d49
mohyeDev <mohye20@gmail.com>
2025-06-19 16:59:26 +0300

    Injecting DbContext

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EgyptWalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EgyptWalksConnectionString")));
 
 var app = builder.Build();
 
