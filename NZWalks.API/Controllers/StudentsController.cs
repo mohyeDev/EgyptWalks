e0b7bcb13fee014455d69a62f58a06821312d209
mohyeDev <mohye20@gmail.com>
2025-06-19 03:11:32 +0300

    Create Domain Models

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsNames = new string[]{ "Mohye","Mohamed","Ahmed","Yasser"};
+
+            return Ok(studentsNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Diffuculty.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Diffuculty
+    {
+
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+
+    }
+}
