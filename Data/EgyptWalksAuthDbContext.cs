cfce5fc23fbc1315eb007cb82fd031f516013296
mohyeDev <mohye20@gmail.com>
2025-07-14 20:04:48 +0300

    Seeding Roles

--- Data/EgyptWalksAuthDbContext.cs
+++ Data/EgyptWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EgyptWalks.Data
@@ -10,6 +11,35 @@ namespace EgyptWalks.Data
         public EgyptWalksAuthDbContext(DbContextOptions<EgyptWalksAuthDbContext> options) : base(options) 
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "76F5E9A9-AF9E-44E5-BD72-E73DC847DE7C",
+                    ConcurrencyStamp = "76F5E9A9-AF9E-44E5-BD72-E73DC847DE7C",
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+
+                },
+
+                 new IdentityRole
+                {
+                    Id = "DB6A88EA-A610-4E36-AC44-005C0161C7B4",
+                    ConcurrencyStamp = "DB6A88EA-A610-4E36-AC44-005C0161C7B4",
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);  
         }
     }
 }
