70d35d23bbd7df048e19b46a26f5d6dbdb346403
mohyeDev <mohye20@gmail.com>
2025-07-16 11:19:46 +0300

    Create Auth Controller and Register Action Method

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using EgyptWalks.Models.DTo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgyptWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser()
+            {
+                UserName = registerRequestDto.UserName,
+                Email = registerRequestDto.UserName
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if(identityResult.Succeeded)
+            {
+                if (registerRequestDto.Roles is not null && registerRequestDto.Roles.Any())
+                {
+                   identityResult =  await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("User was Registered! Please Login.");
+                    }
+                }
+
+
+
+            }
+
+
+            return BadRequest("Something Went Wrong!");
+        }
+    }
+}
---
+++ Models/DTo/RegisterRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EgyptWalks.Models.DTo
+{
+    public class RegisterRequestDto
+    {
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+        [Required]
+
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }    
+
+
+    }
+}
