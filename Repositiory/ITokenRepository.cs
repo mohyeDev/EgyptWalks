399e4e22c1162712d25db996cb88c3cfb4afe236
mohyeDev <mohye20@gmail.com>
2025-07-16 11:35:17 +0300

    Create JWT Token

---
+++ Repositiory/ITokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EgyptWalks.Repositiory
+{
+    public interface ITokenRepository
+    {
+
+        public string CreateJwtToken(IdentityUser user,List<string> roles);
+    }
+}
