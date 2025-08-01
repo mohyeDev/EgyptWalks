19cfeaa164b1b8e16f8e09cbdb2f4be5299c21ae
mohye <mohye20@gmail.com>
2025-08-02 00:18:19 +0300

    Controller and Action Method for Image Upload

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,41 @@
+﻿using EgyptWalks.Models.DTo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgyptWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("Upload")]
+
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto imageUploadRequestDto)
+        {
+            ValidateFileUpload(imageUploadRequestDto);
+
+            if(ModelState.IsValid)
+            {
+
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto imageUploadRequestDto)
+        {
+
+            var allwedExtension = new string[] { ".jpg", ".jpeg", ".png" };
+            if(!allwedExtension.Contains(Path.GetExtension(imageUploadRequestDto.File.FileName)) == false)
+            {
+                ModelState.AddModelError("file","unsupported File Extension!");
+
+            }
+
+            if(imageUploadRequestDto.File.Length> 10485760)
+            {
+                ModelState.AddModelError("File", "FIle Size More Than 10MB");
+            }
+        }
+    }
+}
---
+++ Models/DTo/ImageUploadRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EgyptWalks.Models.DTo
+{
+    public class ImageUploadRequestDto
+    {
+
+        [Required]
+        public IFormFile File { get; set; }
+
+
+        public string FileName { get; set; }
+
+
+        public string? FileDescription { get; set; }
+
+
+
+
+
+    }
+}
